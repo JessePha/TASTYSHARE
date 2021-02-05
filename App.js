8d66f73dc71b1cf184702e10b7165c7c55ef4649
JessePha <ljessepham@hotmail.com>
2021-02-05 11:36:46 +0100

    location permission

--- App.js
+++ App.js
@@ -5,13 +5,32 @@ import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import TastyShare from "./src/TastyShare";
 import store from "./src/shared/global/globalstates/store";
 import SplashScreen from "./src/view/SplashView";
+import * as Location from "expo-location";
 
 function App() {
   const [content, setContent] = useState(<SplashScreen />);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
   useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+  if (errorMsg) {
     const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
     wait(3000).then(() => setContent(<TastyShare />));
-  }, []);
+  } else if (location) {
+    const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    wait(3000).then(() => setContent(<TastyShare />));
+  }
 
   return (
     <SafeAreaView style={styles.AndroidSafeArea}>
